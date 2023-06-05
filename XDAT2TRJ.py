3cf3231dcc05ad2d472c7a5cafb26ca932836d92
cloud-off <118744429+cloud-off@users.noreply.github.com>
2023-06-05 20:47:42 +0700

    Update XDAT2TRJ.py
    convertion to file propre for Avogadro visualisation

--- XDAT2TRJ.py
+++ XDAT2TRJ.py
@@ -1,14 +1,10 @@
 from ase.io import read, write
 import ase.io
-
 # Read XDATCAR file
 atoms_list = read('XDATCAR', index=':')
-
 # Write to .trj file
 with open('output.trj', 'w') as f:
     write('output.trj', atoms_list, format='traj')
-    
 traj = ase.io.read('output.trj', ':')
-
 # Write the trajectory to an XYZ file
 ase.io.write('your_traj_file.xyz', traj)
